bbcae676f9009459f398d716f394bcfa1caed8ff
Jean-Marcel Belmont <mbelmont@us.ibm.com>
2015-10-04 18:22:20 -0400

    Add gitignore and initial files to repository for recipe-maker.

---
+++ app.js
@@ -0,0 +1,66 @@
+Recipes = new Mongo.Collection("recipes");
+
+if (Meteor.isClient) {
+    angular.module('recipes', ['angular-meteor', 'ui.router']);
+
+    angular.module('recipes').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+        function($urlRouterProvider, $stateProvider, $locationProvider) {
+            $locationProvider.html5Mode(true);
+
+            $stateProvider
+                .state('recipes', {
+                    url: '/recipes',
+                    templateUrl: 'recipes-list.ng.html',
+                    controller: 'RecipesListCtrl'
+                })
+                .state('recipeDetails', {
+                    url: '/recipes/:partyId',
+                    templateUrl: 'recipe-details.ng.html',
+                    controller: 'RecipeDetailsCtrl'
+                });
+            $urlRouterProvider.otherwise("/recipes");
+        }
+    ]);
+
+    angular.module('recipes').controller('RecipesListCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+        $scope.recipes = $meteor.collection(Recipes);
+
+        $scope.remove = function(recipe) {
+            $scope.recipes.remove(recipe);
+        };
+
+        $scope.removeAll = function() {
+            $scope.recipes.remove();
+        };
+    }]);
+
+    angular.module("recipes").controller("RecipeDetailsCtrl", ['$scope', '$stateParams',
+        function($scope, $stateParams) {
+            $scope.recipeId = $stateParams.recipeId;
+        }
+    ]);
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        if (Recipes.find().count() === 0) {
+            var recipes = [
+                {
+                    'name': 'Bread 1',
+                    'description': 'This is bread 1.'
+                },
+                {
+                    'name': 'Bread 2',
+                    'description': 'This is bread 2.'
+                },
+                {
+                    'name': 'Bread 3',
+                    'description': 'This is bread 3.'
+                }
+            ];
+            recipes.forEach(function(recipe) {
+               Recipes.insert(recipe);
+            });
+        }
+    });
+}
