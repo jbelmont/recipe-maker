9d0e98b4229f06d5db61e95918ea9181d083ab69
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2015-10-11 20:56:31 -0400

    Fix issue with site not loading.

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,3 +1,13 @@
 angular.module('recipes',
     ['angular-meteor', 'ui.router', 'angularUtils.directives.dirPagination']
 );
+
+function onReady() {
+    angular.bootstrap(document, ['recipes']);
+}
+
+if (Meteor.isCordova) {
+    angular.element(document).on("deviceready", onReady);
+} else {
+    angular.element(document).ready(onReady);
+}
