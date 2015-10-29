b81657a61d54d591039d53aa5c25046a64066694
Jean-Marcel Belmont <mbelmont@us.ibm.com>
2015-10-29 11:19:30 -0400

    Added more sass variables and html and js files for login and create user.

--- client/login.js
+++ client/login.js
@@ -1,4 +1,8 @@
-Accounts.loggingIn(function(user) {
-    var newUser = options;
-
+Template.login.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.loginEmail.value;
+        var passwordVar = event.target.loginPassword.value;
+        console.log("Form submitted.");
+    }
 });
---
+++ client/registerNewUser.js
@@ -0,0 +1,8 @@
+Template.registerNewUser.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var emailVar = event.target.registerEmail.value;
+        var passwordVar = event.target.registerPassword.value;
+        console.log("Form submitted.");
+    }
+});
